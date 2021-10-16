95ceda2c424131e787aa0124129edaab6537d3d9
Valentino Fontanazzi <valenfontanazzi@gmail.com>
2021-10-15 23:43:20 -0300

    feat: update codigos

---
+++ libreria.py
@@ -0,0 +1,225 @@
+from machine import Pin, I2C
+import time
+
+class HCSR04:
+    # echo_timeout_us is based in chip range limit (400cm)
+    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
+        self.echo_timeout_us = echo_timeout_us
+        # Init trigger pin (out)
+        self.trigger = Pin(trigger_pin, mode=Pin.OUT, pull=None)
+        self.trigger.value(0)
+        # Init echo pin (in)
+        self.echo = Pin(echo_pin, mode=Pin.IN, pull=None)
+
+    def _send_pulse_and_wait(self):
+        self.trigger.value(0) # Stabilize the sensor
+        time.sleep_us(5)
+        self.trigger.value(1)
+
+        # Send a 10us pulse.
+        time.sleep_us(10)
+        self.trigger.value(0)
+        try:
+            pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
+            return pulse_time
+        except OSError as ex:
+            if ex.args[0] == 110: # 110 = ETIMEDOUT
+                raise OSError('Out of range')
+            raise ex
+
+    def distance_mm(self):
+        """
+        Get the distance in milimeters without floating point operations.
+        """
+        pulse_time = self._send_pulse_and_wait()
+
+        # To calculate the distance we get the pulse_time and divide it by 2 
+        # (the pulse walk the distance twice) and by 29.1 becasue
+        # the sound speed on air (343.2 m/s), that It's equivalent to
+        # 0.34320 mm/us that is 1mm each 2.91us
+        # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582 
+        mm = pulse_time * 100 // 582
+        return mm
+
+    def distance_cm(self):
+        """
+        Get the distance in centimeters with floating point operations.
+        It returns a float
+
+        """
+        pulse_time = self._send_pulse_and_wait()
+
+        # To calculate the distance we get the pulse_time and divide it by 2 
+        # (the pulse walk the distance twice) and by 29.1 becasue
+        # the sound speed on air (343.2 m/s), that It's equivalent to
+        # 0.034320 cm/us that is 1cm each 29.1us
+        cms = (pulse_time / 2) / 29.1
+        return cms
+
+class Motor:
+  def __init__(self, pin1, pinout1, pin2, pinout2, pin3, pinout3, pin4, pinout4):
+    self.in1 = Pin(pin1, pinout1)
+    self.in2 = Pin(pin2, pinout2)
+    self.in3 = Pin(pin3, pinout3)
+    self.in4 = Pin(pin4, pinout4)
+  
+  def Adelante(self):
+    self.in1.value(0)
+    self.in2.value(1)
+    self.in3.value(0)
+    self.in4.value(1)
+
+  def GirarIzq(self):
+    self.in1.value(1)
+    self.in2.value(0)
+    self.in3.value(0)
+    self.in4.value(1)
+
+  
+  def GirarDer(self):
+    self.in1.value(0)
+    self.in2.value(1)
+    self.in3.value(1)
+    self.in4.value(0)
+
+    
+  def GirarAtras(self):
+    self.Parar()
+    time.sleep(1)
+    self.in1.value(0)
+    self.in2.value(0)
+    self.in3.value(1)
+    self.in4.value(0)
+    time.sleep(20)
+    self.Parar()
+    
+    
+  def Parar(self):
+    self.in1.value(0)
+    self.in2.value(0)
+    self.in3.value(0)
+    self.in4.value(0)
+  
+  def Atras(self):
+    self.Parar()
+    self.in1.value(1)
+    self.in2.value(0)
+    self.in3.value(1)
+    self.in4.value(0)
+
+
+class SensorBase:
+  
+
+	def read16(self, register):
+		data = self.i2c.readfrom_mem(self.address, register, 2)
+		return ustruct.unpack('<H', data)[0]
+
+	def read_temp(self, register):
+		temp = self.read16(register);
+		# apply measurement resolution (0.02 degrees per LSB)
+		temp *= .02;
+		# Kelvin to Celcius
+		temp -= 273.15;
+		return temp;
+
+	def read_ambient_temp(self):
+		return self.read_temp(self._REGISTER_TA)
+
+	def read_object_temp(self):
+		return self.read_temp(self._REGISTER_TOBJ1)
+
+	def read_object2_temp(self):
+		if self.dual_zone:
+			return self.read_temp(self._REGISTER_TOBJ2)
+		else:
+			raise RuntimeError("Device only has one thermopile")
+
+	@property
+	def ambient_temp(self):
+		return self.read_ambient_temp()
+
+	@property
+	def object_temp(self):
+		return self.read_object_temp()
+
+
+	@property
+
+	def object2_temp(self):
+		return self.read_object2_temp()
+
+class MLX90614(SensorBase):
+  
+	_REGISTER_TA = 0x06
+	_REGISTER_TOBJ1 = 0x07
+	_REGISTER_TOBJ2 = 0x08
+
+	def __init__(self, i2c, address=0x5a):
+		self.i2c = i2c
+		self.address = address
+		_config1 = i2c.readfrom_mem(address, 0x25, 2)
+		_dz = ustruct.unpack('<H', _config1)[0] & (1<<6)
+		self.dual_zone = True if _dz else False
+class Stepper:
+    FULL_ROTATION = int(4075.7728395061727 / 8) # http://www.jangeox.be/2013/10/stepper-motor-28byj-48_25.html
+
+    HALF_STEP = [
+        [0, 0, 0, 1],
+        [0, 0, 1, 1],
+        [0, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 1, 0, 0],
+        [1, 1, 0, 0],
+        [1, 0, 0, 0],
+        [1, 0, 0, 1],
+    ]
+
+    FULL_STEP = [
+        [1, 0, 1, 0],
+        [0, 1, 1, 0],
+        [0, 1, 0, 1],
+        [1, 0, 0, 1]
+    ]
+    def __init__(self, mode, pin1, pin2, pin3, pin4, delay):
+    	if mode=='FULL_STEP':
+        	self.mode = self.FULL_STEP
+        else:
+        	self.mode = self.HALF_STEP
+        self.pin1 = pin1
+        self.pin2 = pin2
+        self.pin3 = pin3
+        self.pin4 = pin4
+        self.delay = delay  # Recommend 10+ for FULL_STEP, 1 is OK for HALF_STEP
+        
+        # Initialize all to 0
+        self.reset()
+        
+    def step(self, count, direction=1):
+        """Rotate count steps. direction = -1 means backwards"""
+        if count<0:
+            direction = -1
+            count = -count
+        for x in range(count):
+            for bit in self.mode[::direction]:
+                self.pin1(bit[0])
+                self.pin2(bit[1])
+                self.pin3(bit[2])
+                self.pin4(bit[3])
+                time.sleep_ms(self.delay)
+        self.reset()
+    def angle(self, r, direction=1):
+    	self.step(int(self.FULL_ROTATION * r / 360), direction)
+    def reset(self):
+        # Reset to 0, no holding, these are geared, you can't move them
+        self.pin1(0) 
+        self.pin2(0) 
+        self.pin3(0) 
+        self.pin4(0)
+
+def create(pin1, pin2, pin3, pin4, delay=2, mode='HALF_STEP'):
+	return Stepper(mode, pin1, pin2, pin3, pin4, delay)
+
+
+
+
--- main.py
+++ main.py
@@ -1,238 +1,93 @@
 from machine import Pin, I2C
-
 import network, time, usocket, _thread
-
-class HCSR04:
-    # echo_timeout_us is based in chip range limit (400cm)
-    def __init__(self, trigger_pin, echo_pin, echo_timeout_us=500*2*30):
-        self.echo_timeout_us = echo_timeout_us
-        # Init trigger pin (out)
-        self.trigger = Pin(trigger_pin, mode=Pin.OUT, pull=None)
-        self.trigger.value(0)
-        # Init echo pin (in)
-        self.echo = Pin(echo_pin, mode=Pin.IN, pull=None)
-
-    def _send_pulse_and_wait(self):
-        self.trigger.value(0) # Stabilize the sensor
-        time.sleep_us(5)
-        self.trigger.value(1)
-        # Send a 10us pulse.
-        time.sleep_us(10)
-        self.trigger.value(0)
-        try:
-            pulse_time = machine.time_pulse_us(self.echo, 1, self.echo_timeout_us)
-            return pulse_time
-        except OSError as ex:
-            if ex.args[0] == 110: # 110 = ETIMEDOUT
-                raise OSError('Out of range')
-            raise ex
-
-    def distance_mm(self):
-        """
-        Get the distance in milimeters without floating point operations.
-        """
-        pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2 
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.34320 mm/us that is 1mm each 2.91us
-        # pulse_time // 2 // 2.91 -> pulse_time // 5.82 -> pulse_time * 100 // 582 
-        mm = pulse_time * 100 // 582
-        return mm
-
-    def distance_cm(self):
-        """
-        Get the distance in centimeters with floating point operations.
-        It returns a float
-
-        """
-        pulse_time = self._send_pulse_and_wait()
-
-        # To calculate the distance we get the pulse_time and divide it by 2 
-        # (the pulse walk the distance twice) and by 29.1 becasue
-        # the sound speed on air (343.2 m/s), that It's equivalent to
-        # 0.034320 cm/us that is 1cm each 29.1us
-        cms = (pulse_time / 2) / 29.1
-        return cms
-
-class Motor:
-  def __init__(self, pin1, pinout1, pin2, pinout2, pin3, pinout3, pin4, pinout4):
-    self.in1 = Pin(pin1, pinout1)
-    self.in2 = Pin(pin2, pinout2)
-    self.in3 = Pin(pin3, pinout3)
-    self.in4 = Pin(pin4, pinout4)
-  
-  def Adelante(self):
-    self.in1.value(1)
-    self.in2.value(0)
-    self.in3.value(1)
-    self.in4.value(0)
-
-  def GirarIzq(self):
-    self.Parar()
-    time.sleep(1)
-    self.in1.value(1)
-    self.in2.value(0)
-    self.in3.value(0)
-    self.in4.value(0)
-    time.sleep(10)
-    self.Parar()
-  
-  def GirarDer(self):
-    self.Parar()
-    time.sleep(1)
-    self.in1.value(0)
-    self.in2.value(0)
-    self.in3.value(1)
-    self.in4.value(0)
-    time.sleep(10)
-    self.Parar()
-    
-  def GirarAtras(self):
-    self.Parar()
-    time.sleep(1)
-    self.in1.value(0)
-    self.in2.value(0)
-    self.in3.value(1)
-    self.in4.value(0)
-    time.sleep(20)
-    self.Parar()
-    
-    
-  def Parar(self):
-    self.in1.value(0)
-    self.in2.value(0)
-    self.in3.value(0)
-    self.in4.value(0)
-  
-  def Atras(self):
-    self.Parar()
-    time.sleep(1)
-    self.in1.value(0)
-    self.in2.value(1)
-    self.in3.value(0)
-    self.in4.value(1)
-    time.sleep(3)
-    self.Parar()
-
-class SensorBase:
-  
-
-	def read16(self, register):
-		data = self.i2c.readfrom_mem(self.address, register, 2)
-		return ustruct.unpack('<H', data)[0]
-
-	def read_temp(self, register):
-		temp = self.read16(register);
-		# apply measurement resolution (0.02 degrees per LSB)
-		temp *= .02;
-		# Kelvin to Celcius
-		temp -= 273.15;
-		return temp;
-
-	def read_ambient_temp(self):
-		return self.read_temp(self._REGISTER_TA)
-
-	def read_object_temp(self):
-		return self.read_temp(self._REGISTER_TOBJ1)
-
-	def read_object2_temp(self):
-		if self.dual_zone:
-			return self.read_temp(self._REGISTER_TOBJ2)
-		else:
-			raise RuntimeError("Device only has one thermopile")
-
-	@property
-	def ambient_temp(self):
-		return self.read_ambient_temp()
-
-	@property
-	def object_temp(self):
-		return self.read_object_temp()
-
-	@property
-
-	def object2_temp(self):
-		return self.read_object2_temp()
-
-class MLX90614(SensorBase):
-  
-	_REGISTER_TA = 0x06
-	_REGISTER_TOBJ1 = 0x07
-	_REGISTER_TOBJ2 = 0x08
-
-	def __init__(self, i2c, address=0x5a):
-		self.i2c = i2c
-		self.address = address
-		_config1 = i2c.readfrom_mem(address, 0x25, 2)
-		_dz = ustruct.unpack('<H', _config1)[0] & (1<<6)
-		self.dual_zone = True if _dz else False
-
-
-def wifi():
-  sta_if = network.WLAN(network.STA_IF)
-  if not sta_if.isconnected():
-      print('connecting to network...')
-      sta_if.active(True)
-      sta_if.connect('Avionica-2', 'iMpa2020')
-      while not sta_if.isconnected():
-          pass
-  print('network config:', sta_if.ifconfig()[0])
-
-  s= usocket.socket()
-  s.bind(("192.168.2.38",2020))
-  s.listen(10)
-
-
-  print("Server iniciado, esperando conexiones")
+from libreria import HCSR04, Motor, SensorBase, MLX90614, max30100
+import stepper 
+ 
+motores = Motor(16, Pin.OUT,17, Pin.OUT,18, Pin.OUT,19, Pin.OUT)
+sta_if = network.WLAN(network.STA_IF)
+led=Pin(2,Pin.OUT)  
+sensora1 = HCSR04(trigger_pin=27, echo_pin=26)
+sensora2 = HCSR04(trigger_pin=12, echo_pin=10)
+sensorb3 = HCSR04(trigger_pin=12, echo_pin=13)
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
+sensormlx = MLX90614(i2c) 
+sensormax = max30100.MAX30100(i2c=i2c)
+sensormax.enable_spo2()
+s1 = create(Pin(15,Pin.OUT), Pin(2,Pin.OUT), Pin(0,Pin.OUT), Pin(4,Pin.OUT), delay=2)
+boton = Pin(23, Pin.IN)
 
 def thread1():
-  #control de movimiento
-  motores.Parar()
-  continuar1 = True
-  while continuar1:
-      (sc,addr) = s.accept()
-      print(addr)
-      continuar2=True
-      while continuar2:
-          mensaje = sc.recv(64)
-          print(str(mensaje[0:12]),"primero")
-          if not mensaje:
-              continuar2 = False
-              
-          elif str(mensaje[0:14]) == "b'Adelante()'":
-            print("Adelante()")
-            motores.Parar()
-          
-          elif str(mensaje[0:14]) == "b'GirarIzq()'":
-            print("GirarIzq()")
-            motores.GirarIzq()
-          
-          elif str(mensaje[0:14]) == "b'GirarDer()'":
-            print("GirarDer()")
-            motores.GirarDer()
-          
-          elif str(mensaje[0:16]) == "b'GirarAtras()'":
-            print("GirarAtras()")
-            motores.GirarAtras()
-          
-          elif mensaje == "fin":
-              continuar1 = False
-              print(mensaje.decode())
-          sc.close()
-      s.close()
+  #Blink
+  while True:
+    led.value(1)            #Set led turn on
+    time.sleep(0.5)
+    led.value(0)            #Set led turn off
+    time.sleep(0.5)
 
 def thread2():
-  #control de ultrasonicos
-  distancias = []
-  while True:
-    distancias[0] = round(sensora1.distance_cm())
-    distancias[1] = round(sensora2.distance_cm())
-    distancias[2] = round(sensorb3.distance_cm())
-    time.sleep(1.5)
+  #Control de motores
+  (sc,addr) = s.accept()
+  print(addr)
+  continuar2=True
+  while continuar2:
+    mensaje = sc.recv(64)
+    print(str(mensaje[0:12]),"primero")
+        
+    if str(mensaje[0:14]) == "b'Adelante()'":
+      print("Adelante()")
+      motores.Adelante()
+    
+    elif str(mensaje[0:14]) == "b'GirarIzq()'":
+      print("GirarIzq()")
+      motores.GirarIzq()
+    
+    elif str(mensaje[0:14]) == "b'GirarDer()'":
+      print("GirarDer()")
+      motores.GirarDer()
+    
+    elif str(mensaje[0:16]) == "b'GirarAtras()'":
+      print("GirarAtras()")
+      motores.Atras()
+    
+    elif str(mensaje[0:10]) == "b'Boton()'":
+      if logic_state2 == 1:
+        s1.angle(60)
+        logic_state2 += 1 
+        print("la sube")
+      elif logic_state2 == 2:
+        s1.angle(60,-1)
+        logic_state2 -= 1 
+        print("la baja")
+    
+    elif str(mensaje[0:7]) == "b'Nada'":
+      print("Parar")
+      motores.Parar()
+      
+    
+    if distancea1 <= 30 and distancea1 != 0:
+      motores.Parar()
+      print("Sensor A1 detecto")
+    
+    elif distancea2 <= 30 and distancea2 != 0:
+      motores.Parar()
+      print("Sensor A2 detecto")
+      
+    elif distanceb3 <= 30 and distanceb3 != 0:
+      motores.Parar()
+      print("Sensor B3 detecto")
+      
+    time.sleep(0.6)
+    sc.close()
+  s.close()
 
 def thread3():
+  #Control de ultrasonicos
+  distancea1 = round(sensora1.distance_cm())
+  distancea2 = round(sensora2.distance_cm())
+  distanceb3 = round(sensorb3.distance_cm())
+  time.sleep(0.6)
+
+def thread4():
   #control de sensor de temperatura
   while True:
     contador = 0
@@ -256,24 +111,71 @@ def thread3():
         print("No tenes fiebre y tu temperatura es ", temperature)
     else:
       temperaturas = []
+
+def thread5():
+  #Control sensor oximetro
+  while True:
+    sensormax.read_sensor()
+    #print(sensor.ir, sensor.red)
+    rawspo2 = sensormax.ir
+    rawheartrate = sensormax.red
+
+    spo2 = rawspo2/100
+    heartrate = rawheartrate/200
+    if spo2 > 100 :
+      spo2 = 99.9
+    elif spo2 < 93.0 and spo2 > 50.0 :
+      spo2 = 93.0
+    elif spo2 < 49 :
+      spo2 = 0.0
+    else :
+      spo2 = spo2
+
+    if heartrate > 130 :
+
+      heartrate = 150
+    elif heartrate < 65 and heartrate > 50 :
+      heartrate = 65.0
+    elif heartrate < 49 :
+      heartrate = 0.0
+    else :
+      heartrate = heartrate
+      
+    #os.system("cls")
+    if spo2 > 30:
+      print("Oxigeno en sangre: ", spo2,"%         Ritmo Card閾哸co: ", heartrate)
+    else: 
+      print("Tomando mediciones, porfavor espere.")
   
   
-  
-#Declaracion de pines de motores
-motores = Motor(16, Pin.OUT,17, Pin.OUT,18, Pin.OUT,19, Pin.OUT)
-#Declaracion de pines ultrasonicos
-sensora1 = hcsr04.HCSR04(trigger_pin=27, echo_pin=26)
-sensora2 = hcsr04.HCSR04(trigger_pin=12, echo_pin=10)
-sensorb3 = hcsr04.HCSR04(trigger_pin=12, echo_pin=13)
-#Declaracion de pines de sensor de temperatura 
-i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
-sensormlx = MLX90614(i2c) 
-#Establecer conexion wifi 
-wifi()
-#Arranque de los hilos
+
+
+if not sta_if.isconnected():
+    print('connecting to network...')
+    sta_if.active(True)
+    sta_if.connect('esp', '12345678')
+    while not sta_if.isconnected():
+        pass
+        
+sta_if.ifconfig(('192.168.100.205','255.255.255.0','192.168.100.2','8.8.8.8'))      
+print('network config:', sta_if.ifconfig()[0])
+
+print("Conexion establecida")
+s= usocket.socket()
+s.bind((sta_if.ifconfig()[0],2020))
+print("bindeado")
+s.listen(10)
+
+motores.Parar()
+print("Server iniciado, esperando conexiones")
 _thread.start_new_thread(thread1,())
-_thread.start_new_thread(thread2,())
+
+logic_state2 = 1
+
 _thread.start_new_thread(thread3,())
+_thread.start_new_thread(thread2,())
+_thread.start_new_thread(thread4,())
+_thread.start_new_thread(thread5,())
 
 
 
