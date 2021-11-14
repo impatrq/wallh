a1796d0d642b9e8e4d6658ef0807ef5ebb8a18c7
Valentino Fontanazzi <valenfontanazzi@gmail.com>
2021-11-14 20:38:46 -0300

    feat: update motores

--- motores.py
+++ motores.py
@@ -1,125 +1,116 @@
-from machine import Pin, I2C, UART
 import network, time, usocket, _thread
-from libreria import HCSR04, Motor, create, Stepper, ultrasonicRead
-import max30100
-import json
+from libreria import Motor, MLX90614, readOxTemp, ultrasonicRead, HCSR04, create, Stepper
+import json, max30100
+from machine import Pin, I2C
 
-motores = Motor(16, Pin.OUT,17, Pin.OUT,18, Pin.OUT,19, Pin.OUT)
-sta_if = network.WLAN(network.STA_IF)
-led=Pin(2,Pin.OUT)  
-    
-sensora1 = HCSR04(trigger_pin=27, echo_pin=26)
-sensora2 = HCSR04(trigger_pin=27, echo_pin=10)
-sensorb3 = HCSR04(trigger_pin=27, echo_pin=13)
-s1 = create(Pin(15,Pin.OUT), Pin(1,Pin.OUT), Pin(0,Pin.OUT), Pin(4,Pin.OUT), delay=2)
-
-def thread1():
-  '''Blink'''
+def start(motores, sensormax, sensormlx, sensora1, sensora2, sensorb3, logic_state, s1):
+  server.listen(10)
+  print(f"[LISTENING] Server is listening")
   while True:
-    led.value(1)            #Set led turn on
-    time.sleep(0.5)
-    led.value(0)            #Set led turn off
-    time.sleep(0.5)
+    conn, addr = server.accept()
+    print("aca se para")
+    _thread.start_new_thread(handle_client(conn, addr,motores, sensormax, sensormlx, sensora1, sensora2, sensorb3, logic_state, s1))
+    print(f"[ACTIVE CONNECTIONS]")
+  conn.close()
 
-def thread2(sensora1, sensora2, sensorb3):
-  
-  '''Control de motores con interrupciones de sensor de distancia'''
-  (sc,addr) = s.accept()
-  print(addr)
-  logic_state2 = 1
-  continuar2=True
-  while continuar2:
-    mensaje = sc.recv(64)
-    print(str(mensaje[0:12]),"primero")
+def handle_client(conn, addr, motores, sensormax, sensormlx, sensora1, sensora2, sensorb3, logic_state, s1):
+    print(conn, addr)
+    print("[NEW CONNECTION] connected.")
+
+    temperatura, oxigeno, pulso = readOxTemp(sensormax, sensormlx)
+    
+    mediciones = {
+    'temperatura': temperatura,
+    'oxigeno': oxigeno,
+    'pulso': pulso
+    }
+    
+    while True:
+      mensaje = conn.recv(64).decode(FORMAT)
+
+      if ultrasonicRead(sensora1, sensora2, sensorb3) == "Stop": 
+        pass
+      else:
+        if str(mensaje) == 'Adelante()':
+          print("Adelante()")
+          motores.Adelante()
         
-    if str(mensaje[0:14]) == "b'Adelante()'":
-      print("Adelante()")
-      motores.Adelante()
-    
-    elif str(mensaje[0:14]) == "b'GirarIzq()'":
-      print("GirarIzq()")
-      motores.GirarIzq()
-    
-    elif str(mensaje[0:14]) == "b'GirarDer()'":
-      print("GirarDer()")
-      motores.GirarDer()
-    
-    elif str(mensaje[0:16]) == "b'GirarAtras()'":
-      print("GirarAtras()")
-      motores.Atras()
-    
-    elif str(mensaje[0:10]) == "b'Boton()'":
-      if logic_state2 == 1:
-        s1.angle(60)
-        logic_state2 += 1 
-        print("la sube")
-      elif logic_state2 == 2:
-        s1.angle(60,-1)
-        logic_state2 -= 1 
-        print("la baja")
-    
-    elif str(mensaje[0:7]) == "b'Nada'":
-      print("Parar")
-      motores.Parar()
+        elif str(mensaje[0:14]) == 'GirarIzq()':
+          print("GirarIzq()")
+          motores.GirarIzq()
+        
+        elif str(mensaje[0:14]) == 'GirarDer()':
+          print("GirarDer()")
+          motores.GirarDer()
+        
+        elif str(mensaje[0:16]) == 'GirarAtras()':
+          print("GirarAtras()")
+          motores.Atras()
+        
+        elif str(mensaje[0:10]) == 'Boton()':
+          if logic_state == 1:
+            s1.angle(60)
+            logic_state += 1 
+            print("la sube")
+          elif logic_state == 2:
+            s1.angle(60,-1)
+            logic_state -= 1 
+            print("la baja")
+        
+        elif str(mensaje[0:7]) == 'Nada':
+          print("Parar")
+          motores.Parar()
+       
+      temperatura, oxigeno, pulso = readOxTemp(sensormax, sensormlx)
       
-    distancea1, distancea2, distanceb3 = ultrasonicRead(sensora1, sensora2, sensorb3)
+      mediciones = {
+      'temperatura': temperatura,
+      'oxigeno': oxigeno,
+      'pulso': pulso
+      }
 
-    if distancea1 <= 30 and distancea1 != 0:
-      motores.Parar()
-      print("Sensor A1 detecto")
-    
-    elif distancea2 <= 30 and distancea2 != 0:
-      motores.Parar()
-      print("Sensor A2 detecto")
+      diccionario = json.dumps(mediciones)
+
+      conn.sendall(diccionario.encode(FORMAT))
       
-    elif distanceb3 <= 30 and distanceb3 != 0:
-      motores.Parar()
-      print("Sensor B3 detecto")
-      
-    time.sleep(0.6)
-    sc.close()
-  s.close()
-
-def thread3():
-  '''Recibir datos de sensores de temperatura y oximetro mediante UART'''
-  while True:
-    uart = UART(1, 115200, tx=6, rx=7)    
-    lectura = uart.read()
-    metricas = json.loads(lectura)
+      time.sleep(1)
     
-    temperatura = metricas['temperatura']
-    oxigeno = metricas['oxigeno']
-    pulso = metricas['pulso']
-
-    print("Temperatura: ",temperatura, " Oxigeno en sangre: ", oxigeno, " Pulso: ", pulso)
-    time.sleep(1)
-  
+    conn.close()
 
 
+
+FORMAT = 'utf-8'
+sta_if = network.WLAN(network.STA_IF)
 
 if not sta_if.isconnected():
     print('connecting to network...')
     sta_if.active(True)
-    sta_if.connect('esp', '12345678')
+    sta_if.connect('esp','12345678')
     while not sta_if.isconnected():
         pass
         
-sta_if.ifconfig(('192.168.100.205','255.255.255.0','192.168.100.2','8.8.8.8'))      
+sta_if.ifconfig(('192.168.100.215','255.255.255.0','192.168.100.2','8.8.8.8'))      
 print('network config:', sta_if.ifconfig()[0])
+server= usocket.socket()
 
-print("Conexion establecida")
-s= usocket.socket()
-s.bind((sta_if.ifconfig()[0],2020))
-print("bindeado")
-s.listen(10)
+time.sleep(2)
 
-motores.Parar()
-print("Server iniciado, esperando conexiones")
-_thread.start_new_thread(thread1,())
+server.bind(("192.168.100.215",2020))
+print("binded")
+i2c = I2C(scl=Pin(22), sda=Pin(21), freq=100000)
+sensormlx = MLX90614(i2c) 
+sensormax = max30100.MAX30100(i2c=i2c)
+sensormax.enable_spo2()
+
+motores = Motor(16, Pin.OUT,17, Pin.OUT,18, Pin.OUT,19, Pin.OUT)
+sensora1 = HCSR04(trigger_pin=14, echo_pin=27)
+sensora2 = HCSR04(trigger_pin=14, echo_pin=26)
+sensorb3 = HCSR04(trigger_pin=14, echo_pin=25)
+s1 = create(Pin(15,Pin.OUT), Pin(2,Pin.OUT), Pin(0,Pin.OUT), Pin(4,Pin.OUT), delay=2)
+logic_state = 0
 
 
 
-_thread.start_new_thread(thread3,())
-_thread.start_new_thread(thread2,())
 
-
+print("[STARTING] server is starting...")
+start(motores, sensormax, sensormlx, sensora1, sensora2, sensorb3, logic_state, s1)
